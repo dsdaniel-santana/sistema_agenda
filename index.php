7738b3ba2e8b17005d1984bdc47a4cce6d0181da
Daniel Santana <daniel_santana_87@hotmail.com>
2024-05-15 08:56:38 -0300

    inserindo pagina index

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda das Salas - Senac Tito </title>
+</head>
+<body>
+
+    <div>
+        <h1>Cadastrar</h1>
+        <ul>
+            <li><a href="cadastro_docente.php">Docente</a></li>
+            <li><a href="#">Sub-Area</a></li>
+            <li><a href="#">Curso</a></li>
+            <li><a href="#">Sala</a></li>
+            <li><a href="#">Reserva Salas</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <h1>Listar</h1>
+        <ul>
+            <li><a href="#">Salas Reservadas</a></li>
+            <li><a href="#">Salas Disponiveis</a></li>
+            <li><a href="#">Lista Peridos</a></li>
+            
+        </ul>
+    </div>
+
+
+    
+</body>
+</html>
